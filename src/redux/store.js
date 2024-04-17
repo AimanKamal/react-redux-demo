f185d94aaab09a8c15448c1a5ae4fdc982726779
AimanKamal <22835056+AimanKamal@users.noreply.github.com>
2024-04-17 16:17:59 +0800

    changed createStore fn to configureStore due to deprecation

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,12 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { thunk } from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsDenylist, actionsCreators, serialize...
-      })
-    : compose;
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: () => [logger, thunk],
+  devTools: true
+})
 
 export default store;
